308a0df237aa49bac4af43c6313da0342e0479c5
Hendra Wardana <maxnara19@gmail.com>
2021-04-13 05:31:14 +0700

    implement horizontal extraction

--- block_artifact_grid.py
+++ block_artifact_grid.py
@@ -41,6 +41,23 @@ class Block_Artifact_Grid():
     differ_horz = np.absolute(differ_horz)
     differ_horz = median_filter(differ_horz, size=self.median_filter_size)
 
-    self.result_image = differ_horz
+    for i in range(1, M - 1):
+      for j in range(N):
+        if differ_horz[i, j] > self.differ_treshold:
+          differ_horz[i, j] = self.differ_treshold
+
+    for j in range(N):
+      differ_horz[0, j] = differ_horz[1, j]
+      differ_horz[M - 1, j] = differ_horz[M - 2, j]
+
+    summed_horz = sum_fiter_horz(differ_horz, self.accum_col)
+    summed_horz_pad = mirror_pad_image(summed_horz, self.accum_col // 2, 0)
+    med_vert = median_fiter_vert(summed_horz_pad, self.accum_col)
+
+    edge_horz = summed_horz - med_vert
+    edge_horz_pad = mirror_pad_image(edge_horz, self.accum_col // 2, 0)
+    med_8step_vert = median_filter_8step_vert(edge_horz_pad, self.accum_col)
+
+    self.result_image = med_8step_vert
 
     
--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def mirror_pad_image(image, pad_row, pad_col):
 	padded_img = np.copy(paddedYX)
 
 	for i in range(pad_row):
-		for j in range(np.size(padYX, 1)):
+		for j in range(np.size(paddedYX, 1)):
 			padded_img[i, j] = padded_img[2*pad_row-i, j]
 			padded_img[np.size(paddedYX, 0)-i-1, j] = padded_img[np.size(paddedYX, 0)-2*pad_row+i-1, j]
 
@@ -34,3 +34,68 @@ def mirror_pad_image(image, pad_row, pad_col):
 			padded_img[i, np.size(paddedYX, 1)-j-1] = padded_img[i, np.size(paddedYX, 1)-2*pad_col+j-1]
 	
 	return padded_img
+
+def sum_fiter_horz(image, filter_size):
+	"""
+	Fungsi untuk menjumlah secara horizontal, antar kolom
+	"""
+	offset = filter_size//2
+	img_row = np.size(image, 0)
+	img_col = np.size(image, 1)
+	filtered_img = np.zeros((img_row, img_col-2*offset))
+	
+	for i in range(img_row):
+		sum = 0
+		for jj in range(2*offset+1):
+			sum = sum + image[i, jj]
+			
+		filtered_img[i, 0] = sum
+		for j in range(offset+1, img_col-filter_size + offset):
+			sum = sum - image[i, j-offset-1]
+			sum = sum + image[i, j+offset]
+			filtered_img[i, j-offset] = sum
+		
+	return filtered_img
+
+def median_fiter_vert(image, filter_size):
+  """
+  Fungsi untuk mencari median secara vertical antar row
+  """
+  offset = filter_size//2
+  img_row = np.size(image, 0)
+  img_col = np.size(image, 1)
+  filtered_img = np.zeros((img_row-2*offset, img_col))
+
+  for j in range(img_col):
+    for i in range(img_row-2*offset):
+      counter = 0
+      temp = np.zeros((filter_size - 1))
+
+      for k in range(i, i+filter_size - 1):
+        temp[counter] = image[k, j]
+        counter += 1
+
+      temp = np.sort(temp)
+      filtered_img[i, j] = temp[offset+1]
+
+  return filtered_img
+
+def median_filter_8step_vert(image, filter_size):
+  offset = filter_size//2
+  img_row = np.size(image, 0)
+  img_col = np.size(image, 1)
+  filtered_img = np.zeros((img_row-2*offset, img_col))
+	
+  for i in range(offset, img_row-offset):
+    for j in range(img_col):
+      temp = np.zeros((5))
+      counter = 0
+
+      for k in range(-16, 16, 8):
+        temp[counter] = image[i+k, j]
+        counter = counter + 1
+			
+      temp = np.sort(temp)
+      filtered_img[i-offset, j] = temp[3]
+	
+  return filtered_img
